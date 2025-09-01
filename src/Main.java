c0527427e66b9c67898a4033029a8df2c17be685
Augusto-AJL <augusto_longo@estudante.sesisenai.org.br>
2025-09-01 14:22:28 -0300

    Implementando o Menu Principal
    Implementando o Menu Principal da TaskManagerTrio, com menu interativo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package com.exemplo.taskmanager;
-
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,42 +16,7 @@ public class Main {
             System.out.print("Escolha: ");
             int op = Integer.parseInt(sc.nextLine());
 
-            switch (op) {
-                case 1:
-                    System.out.print("Digite a descrição da tarefa: ");
-                    String desc = sc.nextLine();
-                    manager.adicionarTarefa(desc);
-                    System.out.println("Tarefa adicionada!");
-                    break;
-                case 2:
-                    List<Tarefa> lista = manager.listarTarefas();
-                    if (lista.isEmpty()) {
-                        System.out.println("Nenhuma tarefa cadastrada.");
-                    } else {
-                        for (int i = 0; i < lista.size(); i++) {
-                            System.out.println(i + ") " + lista.get(i));
-                        }
-                    }
-                    break;
-                case 3:
-                    System.out.print("Informe o índice da tarefa para remover: ");
-                    int idx = Integer.parseInt(sc.nextLine());
-                    boolean removida = manager.removerTarefa(idx);
-                    if (removida) {
-                        System.out.println("Tarefa removida com sucesso.");
-                    } else {
-                        System.out.println("Índice inválido.");
-                    }
-                    break;
-                case 0:
-                    sair = true;
-                    break;
-                default:
-                    System.out.println("Opção inválida.");
-            }
-        }
 
-        sc.close();
-        System.out.println("Programa encerrado.");
+        }
     }
 }
